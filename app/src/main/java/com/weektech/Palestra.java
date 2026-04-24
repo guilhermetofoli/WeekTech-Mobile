f5df0ebcac320d24e7906ff611f0cfde97ff867e
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-23 22:02:04 -0300

    Adicionado classe model Palestra para estruturação de dados

--- app/src/main/java/com/weektech/Palestra.java
+++ app/src/main/java/com/weektech/Palestra.java
@@ -1,4 +1,20 @@
 package com.weektech;
 
 public class Palestra {
+    private String titulo, palestrante, data, local;
+    private int fotoResId;
+
+    public Palestra(String titulo, String palestrante, String data, String local, int fotoResId) {
+        this.titulo = titulo;
+        this.palestrante = palestrante;
+        this.data = data;
+        this.local = local;
+        this.fotoResId = fotoResId;
+    }
+
+    public String getTitulo() { return titulo; }
+    public String getPalestrante() { return palestrante; }
+    public String getData() { return data; }
+    public String getLocal() { return local; }
+    public int getFotoResId() { return fotoResId; }
 }
