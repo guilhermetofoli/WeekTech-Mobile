dce9d5c79fba9c11988c44722a16c5e3aa3b7534
Guilherme Tófoli <guitofoli1806@gmail.com>
2026-04-23 22:02:51 -0300

    Adicionado lógica da tela de registro com campos acadêmicos

--- app/src/main/java/com/weektech/RegisterActivity.java
+++ app/src/main/java/com/weektech/RegisterActivity.java
@@ -1,4 +1,12 @@
 package com.weektech;
 
-public class MainActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
 }
