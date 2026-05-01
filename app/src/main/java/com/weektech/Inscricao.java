091d2d51518f7835d95cd2c8eeb9143fa03125fe
Guizorrr <guitofoli1806@gmail.com>
2026-05-01 16:06:51 -0300

    Criada entidade Inscricao para persistência de matrículas no banco de dados

--- app/src/main/java/com/weektech/Inscricao.java
+++ app/src/main/java/com/weektech/Inscricao.java
@@ -1,4 +1,18 @@
 package com.weektech;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "inscricoes")
 public class Inscricao {
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String raUsuario;
+    public int palestraId;
+
+    public Inscricao(String raUsuario, int palestraId) {
+        this.raUsuario = raUsuario;
+        this.palestraId = palestraId;
+    }
 }
