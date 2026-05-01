075f10878759d20f987b2998463a1db9565f7599
Guizorrr <guitofoli1806@gmail.com>
2026-05-01 16:11:46 -0300

    Adicionado método isAdmin e controle de nível de acesso na sessão do usuário

--- app/src/main/java/com/weektech/util/SessionManager.java
+++ app/src/main/java/com/weektech/util/SessionManager.java
@@ -1,4 +1,39 @@
 package com.weektech.util;
 
+import android.content.Context;
+import android.content.SharedPreferences;
+
 public class SessionManager {
+    private static final String PREF_NAME = "WeekTechSession";
+    private static final String KEY_IS_LOGGED_IN = "isLoggedIn";
+    private static final String KEY_IS_ADMIN = "isAdmin"; // Nova chave
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+
+    public SessionManager(Context context) {
+        pref = context.getSharedPreferences(PREF_NAME, Context.MODE_PRIVATE);
+        editor = pref.edit();
+    }
+
+    //
+    public void createLoginSession(boolean isAdmin) {
+        editor.putBoolean(KEY_IS_LOGGED_IN, true);
+        editor.putBoolean(KEY_IS_ADMIN, isAdmin);
+        editor.apply();
+    }
+
+    public boolean isLoggedIn() {
+        return pref.getBoolean(KEY_IS_LOGGED_IN, false);
+    }
+
+    //
+    public boolean isAdmin() {
+        return pref.getBoolean(KEY_IS_ADMIN, false);
+    }
+
+    public void logout() {
+        editor.clear();
+        editor.apply();
+    }
 }
