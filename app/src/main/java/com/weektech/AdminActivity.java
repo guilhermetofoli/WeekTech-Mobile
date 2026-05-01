a8cec175f30b3f3aa12284748dc818c39236ae96
Guizorrr <guitofoli1806@gmail.com>
2026-05-01 16:10:01 -0300

    Implementada lógica de salvamento e gerenciamento de palestras via Room

--- app/src/main/java/com/weektech/AdminActivity.java
+++ app/src/main/java/com/weektech/AdminActivity.java
@@ -1,10 +1,9 @@
 package com.weektech;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,10 +11,8 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.textfield.TextInputEditText;
-import com.weektech.adapter.AdminPalestraAdapter;
-import com.weektech.database.AppDatabase;
-import com.weektech.database.dao.PalestraDao;
-import com.weektech.model.Palestra;
+import com.weektech.AppDatabase;
+import com.weektech.Palestra;
 
 public class AdminActivity extends AppCompatActivity
         implements AdminPalestraAdapter.OnAdminActionListener {
