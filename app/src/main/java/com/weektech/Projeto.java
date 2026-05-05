31074fb582e48f643606733ccbf35e7b5be8e5cd
LayonAdS <locosilia5@gmail.com>
2026-05-04 21:03:39 -0300

    Adiciona entidade Projeto no Room com estrutura de dados contendo título, descrição, ID do usuário responsável e data de submissão

--- app/src/main/java/com/weektech/Projeto.java
+++ app/src/main/java/com/weektech/Projeto.java
@@ -1,4 +1,25 @@
 package com.weektech;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "projetos")
 public class Projeto {
+
+    @PrimaryKey(autoGenerate = true)
+    public int id;
+
+    public String nome;
+    public String ra;
+    public String nomeProjeto;
+    public String descricao;
+
+    public Projeto() {}
+
+    public Projeto(String nome, String ra, String nomeProjeto, String descricao) {
+        this.nome = nome;
+        this.ra = ra;
+        this.nomeProjeto = nomeProjeto;
+        this.descricao = descricao;
+    }
 }
