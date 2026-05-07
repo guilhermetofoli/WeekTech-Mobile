c3ad032d4cba96172700a58d847d414a4694b910
LayonAdS <locosilia5@gmail.com>
2026-05-07 20:59:53 -0300

    adicionar método listarAgendados e atualizar método update para permitir que alunos visualizem apenas projetos com data de apresentação definida.

--- app/src/main/java/com/weektech/ProjetoDao.java
+++ app/src/main/java/com/weektech/ProjetoDao.java
@@ -3,6 +3,8 @@ package com.weektech;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
+import androidx.room.Update;
+
 import java.util.List;
 
 @Dao
@@ -11,8 +13,14 @@ public interface ProjetoDao {
     @Insert
     long inserir(Projeto projeto);
 
+    @Update
+    void update(Projeto projeto);
+
     @Query("SELECT * FROM projetos ORDER BY nomeProjeto ASC")
     List<Projeto> listarTodos();
+
+    @Query("SELECT * FROM projetos WHERE dataApresentacao IS NOT NULL AND dataApresentacao != '' ORDER BY dataApresentacao, horaApresentacao ASC")
+    List<Projeto> listarAgendados();
 
     @Query("SELECT COUNT(*) FROM projetos WHERE ra = :ra")
     int verificarProjetoExistente(String ra);
