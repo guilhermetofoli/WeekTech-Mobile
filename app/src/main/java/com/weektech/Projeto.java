b4e5f75c91200c9ba4e6a331be732cfe04624514
LayonAdS <locosilia5@gmail.com>
2026-05-07 20:59:36 -0300

    adicionar campos dataCriacao e horaCriacao, além de construtores de compatibilidade e métodos getTitulo e getAlunoId para rastrear envio de projetos e permitir agendamento pelo administrador.

--- app/src/main/java/com/weektech/Projeto.java
+++ app/src/main/java/com/weektech/Projeto.java
@@ -14,6 +14,13 @@ public class Projeto {
     public String nomeProjeto;
     public String descricao;
 
+    public String dataApresentacao;
+    public String horaApresentacao;
+
+    // Campos de auditoria/criação
+    public String dataCriacao;
+    public String horaCriacao;
+
     public Projeto() {}
 
     public Projeto(String nome, String ra, String nomeProjeto, String descricao) {
@@ -21,5 +28,84 @@ public class Projeto {
         this.ra = ra;
         this.nomeProjeto = nomeProjeto;
         this.descricao = descricao;
+    }
+
+    public Projeto(String nome, String ra, String nomeProjeto,
+                   String descricao, String dataApresentacao,
+                   String horaApresentacao) {
+
+        this.nome = nome;
+        this.ra = ra;
+        this.nomeProjeto = nomeProjeto;
+        this.descricao = descricao;
+        this.dataApresentacao = dataApresentacao;
+        this.horaApresentacao = horaApresentacao;
+    }
+
+    // GETTERS E SETTERS
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRa() {
+        return ra;
+    }
+
+    public void setRa(String ra) {
+        this.ra = ra;
+    }
+
+    public String getNomeProjeto() {
+        return nomeProjeto;
+    }
+
+    public void setNomeProjeto(String nomeProjeto) {
+        this.nomeProjeto = nomeProjeto;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDataApresentacao() {
+        return dataApresentacao;
+    }
+
+    public void setDataApresentacao(String dataApresentacao) {
+        this.dataApresentacao = dataApresentacao;
+    }
+
+    public String getHoraApresentacao() {
+        return horaApresentacao;
+    }
+
+    public void setHoraApresentacao(String horaApresentacao) {
+        this.horaApresentacao = horaApresentacao;
+    }
+
+    // Aliases for adapter
+    public String getTitulo() {
+        return nomeProjeto;
+    }
+
+    public String getAlunoId() {
+        return ra;
     }
 }
